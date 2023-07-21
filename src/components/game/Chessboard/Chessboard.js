820df2b3419c47eea3a0a0b098baa31072fbb86e
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-07-21 03:19:56 -0300

    feat(chessboard): empty board matrix

--- src/components/game/Chessboard/Chessboard.js
+++ src/components/game/Chessboard/Chessboard.js
@@ -1,25 +1,35 @@
-import React from "react"
-import Tile from "../../common/Tile/Tile"
-import "./Chessboard.css"
+import React from "react";
+import Tile from "../../common/Tile/Tile";
+import "./Chessboard.css";
 
 const Chessboard = () => {
-  const rows = Array.from({ length: 8 }, (_, row) => row)
-  const cols = Array.from({ length: 8 }, (_, col) => col)
+    const boardCells = [];
+    for (let row = 0; row < 8; row++) {
+        boardCells.push([]);
+        for (let col = 0; col < 8; col++) {
+            boardCells[row].push({
+                empty: true,
+                piece: null,
+                row,
+                col,
+            });
+        }
+    }
 
-  return (
-    <div className="chessboard">
-      {rows.map((row) =>
-        cols.map((col) => (
-          <Tile
-            key={`${row}-${col}`}
-            isDark={(row + col) % 2 === 1}
-            row={row}
-            col={col}
-          />
-        ))
-      )}
-    </div>
-  )
-}
+    return (
+        <div className="chessboard">
+            {boardCells.map((row) =>
+                row.map((cell) => (
+                    <Tile
+                        key={`${cell.row}-${cell.col}`}
+                        isDark={(cell.row + cell.col) % 2 === 1}
+                        row={cell.row}
+                        col={cell.col}
+                    />
+                ))
+            )}
+        </div>
+    );
+};
 
-export default Chessboard
+export default Chessboard;
