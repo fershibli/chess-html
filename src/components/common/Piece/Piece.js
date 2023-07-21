fafe3ad01328ac84ef286b3a21185ed5e0a33644
FernandoLuizNemeChibli <fernandoluiz.1991@gmail.com>
2023-07-21 03:55:23 -0300

    feat(Tile, Piece): render pieces and coordinates

--- src/components/common/Piece/Piece.js
+++ src/components/common/Piece/Piece.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Piece.css";
 
-const Piece = ({ char }) => {
-    if (char) {
-        return <div className="piece">{char}</div>;
-    } else {
-        return <div></div>;
-    }
+const Piece = ({ char, col, row }) => {
+    return (
+        <div style={{ position: "relative" }}>
+            <span
+                style={{
+                    position: "absolute",
+                    color: "gray",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                }}
+            >
+                {row + 1}, {col + 1}
+            </span>
+            {char && <div className="piece">{char}</div>}
+        </div>
+    );
 };
 
 export default Piece;
--- src/components/common/Tile/Tile.js
+++ src/components/common/Tile/Tile.js
@@ -7,7 +7,7 @@ const Tile = ({ tile, isDark }) => {
 
     return (
         <button className={`tile ${tileClass}`}>
-            <Piece char={tile.piece?.dark} />
+            <Piece char={tile.piece?.dark} col={tile.col} row={tile.row} />
         </button>
     );
 };
